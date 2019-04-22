7b6cdb6f048312a92d9917eca3167bbc1de6d064
Adam Grise <adamwgrise@gmail.com>
2019-04-21 23:33:50 -0500

    A bit more done with the javascript, but this is really hard. I'm not sure I'm doing any of this right.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,19 +1,39 @@
 // these are always the set of available solutions, available globally
-var solutions = ["JIGSAW","TABLESAW","POWERDRILL","HAMMER","SCREWDRIVER","SANDPAPER"]
+var solutions = ["jigsaw", "tablesaw", "powerdrill", "hammer", "screwdriver", "sandpaper"]
+
+var letters = "abcdefghijklmnopqrstuvwxyz";
 
 // something needs to select the random word when the user starts the game,
 // then the number of available guesses needs to be populated
 
 // this just needs to be reset at the start of each round
-var guesses = []
+var guessesLeft, guessedLetters, currentSolutionWord, currentSolutionArray;
 
-// this should be run only once the round has started
-document.onkeyup = function(event) {
-    var letter = event.key.toUpperCase();
-    if (letter === "already guessed") {
-        //do nothing
-    }else if (letter === "match"){
-        //do something good
-    }else if (letter === "no match"){
-        //do something bad
-    };
+/********* GAME SETUP ***********/
+var setup = function () {
+    currentSolutionWord = solutions[Math.floor(Math.random() * solutions.length)];
+    guessesLeft = currentSolutionWord.length + 2;
+    currentSolutionArray = currentSolutionWord.split("");
+    console.log("currentSolutionWord: " + currentSolutionWord + " | currentSolutionArray: " + currentSolutionArray)
+    guessedLetters = []
+}
+
+// this should be running only once the round has started
+document.onkeyup = function (event) {
+    var letter = event.key.toLowerCase();
+    if (letters.indexOf(letter) > -1) {
+        alert("Pick a letter!")
+    } else if (letter === "already guessed") {
+        alert("You've already guessed the letter " + letter + "!")
+    } else if (letter === "match") {
+        // do something good
+    } else if (letter === "no match") {
+        // do something bad
+    }
+};
+
+
+// on page load, run script
+document.onload = function () {
+    setup();
+};
