b8a06ff58e44b78564137607596f1f820a41a85d
Adam Grise <adamwgrise@gmail.com>
2019-04-27 16:41:19 -0500

    Lots of progress, sorry I didn't commit often enough to reflect changes

--- assets/javascript/jQueryVersion.js
+++ assets/javascript/jQueryVersion.js
@@ -1,26 +1,26 @@
 var possibleSolutions = ["tablesaw", "powerdrill", "jigsaw", "bandsaw"];
 var roundSolution = [];
 var roundSolutionBlanks = [];
-var guessLetter;
-var guessIs = null;
+var guessCorrect = null;
+var guessLetter = null;
 var win = 0;
-var guessLog = [];
-var totalTurns = null;
+var guessesLog = [];
 var turnsLeft = null;
+var guessDupe = 0
 
+//// FUNCTIONS FOR PREPPING THE ROUND
 var freshRound = function () {
     roundSolution = [];
     roundSolutionBlanks = [];
     guessLetter = null;
-    guessIs = null;
-    guessLog = [];
-    totalTurns = null;
+    guessCorrect = null;
+    guessesLog.length = 0;
     turnsLeft = null;
 }
 
-// Just runs at the beginning of the round
 var getSolution = function () {
     word = possibleSolutions[Math.floor(Math.random() * possibleSolutions.length)]
+    word = word.toUpperCase();
     roundSolution = word.split("");
     console.log("Round solution chosen: " + roundSolution + " (" + roundSolution.length + " letters)")
 };
@@ -29,102 +29,113 @@ var genBlanks = function () {
     for (i = 0; i < roundSolution.length; i++) {
         roundSolutionBlanks.push("_");
     }
+    $("#theWord").text(roundSolutionBlanks);
 }
 
-// This needs to run for each guess the user makes
+//// FUNCTIONS FOR EACH USER GUESS
 
 var guessDupeNLog = function () {
     console.log("Guessed letter: " + guessLetter);
-    if (guessLog.includes(guessLetter)) {
+    if (guessesLog.includes(guessLetter)) {
         alert("YOU CLOWN, YOU ALREADY GUESSED THAT LETTER");
+        guessDupe = 1;
     } else {
-        guessLog.push(guessLetter);
+        guessesLog.push(guessLetter);
+        guessDupe = 0;
     }
-    console.log("Guess Log: " + guessLog);
+    console.log("Guess Log: " + guessesLog);
 }
 
 var guessMatch = function () {
     if (roundSolution.includes(guessLetter)) {
-        guessIs = 1;
+        guessCorrect = 1;
     } else {
-        guessIs = 0;
+        guessCorrect = 0;
         turnsLeft--;
     }
     console.log("Turns left: " + turnsLeft);
 }
 
-var reveal = function () {
-    if (guessIs === 1) {
+var guessRevealOrLose = function () {
+    if (guessCorrect === 1) {
         for (i = 0; i < roundSolution.length; i++) {
             if (guessLetter === roundSolution[i]) {
-                console.log("Before reveal: " + roundSolutionBlanks);
                 roundSolutionBlanks[i] = guessLetter;
                 console.log("After reveal: " + roundSolutionBlanks);
             }
         }
+    } else if (guessCorrect === 1 && turnsLeft === 0) {
+        document.getElementById("submitGuess").disabled = true;
+        alert("You lose! Bummer.");
+        // Disable the submit button, only allow new game to start.
+    } else {
+        // No action
     }
 }
 
-var isWin = function () {
+//// WIN CHECK
+
+var guessIsWin = function () {
     if (roundSolutionBlanks.includes("_")) {
-        win = 0;
+        // No action; not a win yet.
     } else {
-        win = 1;
+        youWin();
     }
 }
+
+//// USER WINS
 
 var youWin = function () {
     console.log("Win confirmed");
     win = 0;
-    // do other stuff to reset the game
-    alert("You won! Press OK to restart.");
-    freshRound();
-    runGame();
+    // Replace this alert, don't restart round until a restart button is pushed
+    document.getElementById("submitGuess").disabled = true;
+    alert("You won! Push the Start a New Game button to begin a new round.");
+    // Disable the submit button until new round starts
 }
 
 //////////// Actual stuff -happening- /////////////////
 
-// New Game
-var runGame = function () {
+// New Game Setup
+
+var gameSetup = function () {
+    freshRound();
     getSolution();
     genBlanks();
-
-    totalTurns = roundSolution.length;
-    turnsLeft = totalTurns;
-
-    console.log("Total turns: " + totalTurns);
-
-    for (i = 0; i < 10; i++) {
-        if (win === 0) {
-            if (turnsLeft === 0) {
-                alert("You lose! Click OK to try again.");
-                freshRound();
-                runGame();
-                break;
-            } else {
-                guessLetter = prompt("Guess a letter!");
-                guessLetter = guessLetter.toLowerCase();
-                guessDupeNLog();
-                guessMatch();
-                reveal();
-                isWin();
-            }
-        } else {
-            youWin();
-            break;
-        };
-        // Logging
-        console.log("Guess: " + guessLetter + ", " + guessIs);
-        console.log("Win? " + win);
-    }
+    turnsLeft = roundSolution.length;
+    document.getElementById("submitGuess").disabled = false; 
 }
+
+// Control for the text field - only allow letters
+document.getElementById("currentGuess").onkeyup = function (event) {
+    this.value = this.value.replace(/[^a-zA-Z]/gi, '');
+}
+
+// Functions for each guess submitted
+
+var runGame = function () {
+    guessLetter = document.getElementById('currentGuess').value;
+    guessLetter = guessLetter.toUpperCase();
+    guessDupeNLog();
+    guessMatch();
+    guessRevealOrLose();
+    $("#theWord").text(roundSolutionBlanks);
+    guessIsWin();
+}
+
+// Elements to show on-screen
+
 
 // Page loads & runs game
 
-runGame();
+gameSetup();
 
 /********* to do
 
-catalog and display guesses
+catalog and display guesses fancily
+show turns left
+clear text box on guess
+stop decrementing remaining turns on duplicate guess
+interrupt runGame if duplicate guess
 
 */
